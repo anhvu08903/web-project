a74890e54339422b0a6ef1d2d59bd06574510a3a
ngoc vu <vungocanhdocac@gmail.com>
2024-03-28 21:38:28 +0700

    first.commit

---
+++ project-web-backend/src/main/java/com/example/projectwebbackend/ProjectWebBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectwebbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectWebBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectWebBackendApplication.class, args);
+    }
+
+}
