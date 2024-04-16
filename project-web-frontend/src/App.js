5fad7f61935ed9cd7c20877cd6edc89163913d0b
TAnk <Quachhuutunganh194@gmail.com>
2024-04-16 21:06:26 +0700

    Homepage fixed

--- project-web-frontend/src/App.js
+++ project-web-frontend/src/App.js
@@ -9,39 +9,45 @@ import RangeSlider from "./Components/Booking/RangeSlider";
 import ToggleableRangeSlider from "./Components/Booking/testSlider";
 import Homepage from "./Components/Homepage/Homepage";
 
-function App() {
-  const [userState, setUserState] = useState({});
-  return (
-    <div className="App">
-      <Router>
-        <Routes>
-          <Route
-            path="/"
-            element={
-              userState && userState._id ? (
-                <Profile
-                  setUserState={setUserState}
-                  username={userState.fname}
-                />
-              ) : (
-                <Login setUserState={setUserState} />
-              )
-            }
-          ></Route>
-          <Route
-            path="/login"
-            element={<Login setUserState={setUserState} />}
-          ></Route>
-          <Route path="/signup" element={<Register />}></Route>
-          <Route path = "/booking" element={<Booking />}></Route>
-          <Route path = "/slider" element={<RangeSlider />}></Route>
-          <Route path = "/testslider" element={<ToggleableRangeSlider />}></Route>
-          <Route path="/homepage" element={<Homepage />}></Route>
+// function App() {
+//   const [userState, setUserState] = useState({});
+//   return (
+//     <div className="App">
+//       <Router>
+//         <Routes>
+//           <Route
+//             path="/"
+//             element={
+//               userState && userState._id ? (
+//                 <Profile
+//                   setUserState={setUserState}
+//                   username={userState.fname}
+//                 />
+//               ) : (
+//                 <Login setUserState={setUserState} />
+//               )
+//             }
+//           ></Route>
+//           <Route
+//             path="/login"
+//             element={<Login setUserState={setUserState} />}
+//           ></Route>
+//           <Route path="/signup" element={<Register />}></Route>
+//           <Route path = "/booking" element={<Booking />}></Route>
+//           <Route path = "/slider" element={<RangeSlider />}></Route>
+//           <Route path = "/testslider" element={<ToggleableRangeSlider />}></Route>
+//           <Route path="/homepage" element={<Homepage />}></Route>
 
           
-        </Routes>
-      </Router>
-    </div>
+//         </Routes>
+//       </Router>
+//     </div>
+//   );
+// }
+
+function App() {
+  return (
+    <Homepage></Homepage>
   );
 }
 
--- project-web-frontend/src/Components/Homepage/Homepage.js
+++ project-web-frontend/src/Components/Homepage/Homepage.js
@@ -1,59 +1,61 @@
 import React from 'react';
-import './Homepage.module.css';
+import styles from './Homepage.module.css';
 import { useNavigate } from 'react-router-dom';
 
-const Homepage = () => {
-  const navigate = useNavigate();
 
-  const handleLoginButtonClick = () => {
-    navigate('/login');
-  };
+
+const Homepage = () => {
+  // const navigate = useNavigate();
+
+  // const handleLoginButtonClick = () => {
+  //   navigate('/login');
+  // };
 
 
   return (
     <div>
-      <div className="navbar">
-        <div className="header-left">
+      <div className={styles.navbar}>
+        <div className={styles.headerLeft}>
 
         </div>
-        <ul className="header-right">
+        <ul className={styles.headerRight}>
           <li>Đăng ký mở bán vé</li>
-          <div className="sign-in-button">
-            <button className="buttons">
+          <div className={styles.signInButton}>
+            <button className={styles.buttons}>
               <i className="material-icons-round">phone</i>
               Hotline 24/7
               <span></span>
             </button>
             {/* thêm chức năng xử lý route vào button */}
-            <button className="buttons" onClick={handleLoginButtonClick}>
+            <button className={styles.buttons} /*onClick={handleLoginButtonClick}*/>
               Đăng nhập
               <span></span>
             </button>
           </div>
         </ul>
       </div>
-      <div className="banner">
+      <div className={styles.banner}>
         {/* test ảnh vì bị lỗi homepage */}
-        <img className="banner-background" src="https://229a2c9fe669f7b.cmccloud.com.vn/images/banner-main-vi.jpg" alt="Banner background"  style={{height: 100}}/>
-        <div className="banner-middle">
+        <img className={styles.bannerBackground} src="https://229a2c9fe669f7b.cmccloud.com.vn/images/banner-main-vi.jpg"/>
+        <div className={styles.bannerMiddle}>
           <h2>Đặt vé xe đơn giản và an toàn</h2>
-          <div className="ticket-widget">
-            <div className="info-row">
-              <div className="info" id="start-point">
+          <div className={styles.ticketWidget}>
+            <div className={styles.infoRow}>
+              <div className={styles.info} id={styles.startPoint}>
 
               </div>
-              <div className="info" id="end-point">
+              <div className={styles.info} id={styles.endPoint}>
  
               </div>
-              <div className="info" id="start-date">
+              <div className={styles.info} id={styles.startDate}>
    
               </div>
-              <div className="info" id="end-date">
+              <div className={styles.info} id={styles.endDate}>
    
               </div>
             </div>
-            <div className="search-button">
-              <button className="buttons" id="search-button">
+            <div className={styles.searchButton}>
+              <button className={styles.buttons} id={styles.searchButton}>
                 Tìm kiếm chuyến xe ngay
                 <span></span>
               </button>
@@ -61,24 +63,24 @@ const Homepage = () => {
           </div>
         </div>
       </div>
-      <div className="end">
-        <div className="background"></div>
-        <div className="end-items">
-          <div className="items">
-            <img className="item-img" src="https://229a2c9fe669f7b.cmccloud.com.vn/svgIcon/verified_yellow.svg" alt="Verified" />
-            <p className="title">Chắc chắn có chỗ</p>
+      <div className={styles.end}>
+        <div className={styles.background}></div>
+        <div className={styles.endItems}>
+          <div className={styles.items}>
+            <img className={styles.itemImg} src="https://229a2c9fe669f7b.cmccloud.com.vn/svgIcon/verified_yellow.svg" alt="Verified" />
+            <p className={styles.title}>Chắc chắn có chỗ</p>
           </div>
-          <div className="items">
-            <img className="item-img" src="https://229a2c9fe669f7b.cmccloud.com.vn/svgIcon/headset_mic_yellow.svg" alt="Headset" />
-            <p className="title">Hỗ trợ 24/7</p>
+          <div className={styles.items}>
+            <img className={styles.itemImg} src="https://229a2c9fe669f7b.cmccloud.com.vn/svgIcon/headset_mic_yellow.svg" alt="Headset" />
+            <p className={styles.title}>Hỗ trợ 24/7</p>
           </div>
-          <div className="items">
-            <img className="item-img" src="https://229a2c9fe669f7b.cmccloud.com.vn/svgIcon/discount_yellow.svg" alt="Discount" />
-            <p className="title">Nhiều ưu đãi</p>
+          <div className={styles.items}>
+            <img className={styles.itemImg} src="https://229a2c9fe669f7b.cmccloud.com.vn/svgIcon/discount_yellow.svg" alt="Discount" />
+            <p className={styles.title}>Nhiều ưu đãi</p>
           </div>
-          <div className="items">
-            <img className="item-img" src="https://229a2c9fe669f7b.cmccloud.com.vn/svgIcon/monetization_on_yellow.svg" alt="Payment" />
-            <p className="title">Thanh toán đa dạng</p>
+          <div className={styles.items}>
+            <img className={styles.itemImg} src="https://229a2c9fe669f7b.cmccloud.com.vn/svgIcon/monetization_on_yellow.svg" alt="Payment" />
+            <p className={styles.title}>Thanh toán đa dạng</p>
           </div>
         </div>
       </div>
