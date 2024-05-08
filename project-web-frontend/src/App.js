8ef04dc5cd35e83d5b5db3dbaeb591f96f05ccfa
ngoc vu <vungocanhdocac@gmail.com>
2024-05-08 18:59:59 +0700

    fixcoach

--- project-web-frontend/src/App.js
+++ project-web-frontend/src/App.js
@@ -2,7 +2,13 @@ import "./App.css";
 import Profile from "./Components/Profile/Profile";
 import Login from "./Components/Login/Login";
 import Register from "./Components/Register/Register";
-import { BrowserRouter as Router, Routes, Route, BrowserRouter, Navigate } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  BrowserRouter,
+  Navigate,
+} from "react-router-dom";
 import { useState } from "react";
 import Booking from "./Components/Booking/Booking";
 import Homepage from "./Components/Homepage/Homepage";
@@ -11,7 +17,7 @@ import Coach from "./Components/Coach/Coach";
 import LoginCoach from "./Components/Login/LoginCoach";
 import RegisterCoach from "./Components/Register/RegisterCoach";
 import AdminLogin from "./Components/Login/AdminLogin";
-import AdminRegister from "./Components/Register/AdmibRegister";
+import AdminRegister from "./Components/Register/AdminRegister";
 
 // function App() {
 //   const [userState, setUserState] = useState({});
@@ -42,22 +48,23 @@ import AdminRegister from "./Components/Register/AdmibRegister";
 //           <Route path = "/testslider" element={<ToggleableRangeSlider />}></Route>
 //           <Route path="/homepage" element={<Homepage />}></Route>
 
-          
 //         </Routes>
 //       </Router>
 //     </div>
 //   );
 // }
 
-{/* Tớ thay đổi cái này chút để test Homepage, có gì cậu cứ sửa lại nhé! - TAnk */}
+{
+  /* Tớ thay đổi cái này chút để test Homepage, có gì cậu cứ sửa lại nhé! - TAnk */
+}
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/login" element={<Login />}/>
-          <Route path="/adminlogin" element={<AdminLogin />}/>
+          <Route path="/login" element={<Login />} />
+          <Route path="/adminlogin" element={<AdminLogin />} />
           <Route path="/signup" element={<Register />}></Route>
           <Route path="/adminsignup" element={<AdminRegister />}></Route>
           <Route path="/booking" element={<Booking />}></Route>
@@ -66,7 +73,7 @@ function App() {
           <Route path="/coach" element={<Coach />}></Route>
           <Route path="/logincoach" element={<LoginCoach />}></Route>
           <Route path="/registercoach" element={<RegisterCoach />}></Route>
-          <Route path="/" element ={<Navigate replace to="/homepage"/>}></Route>
+          <Route path="/" element={<Navigate replace to="/homepage" />}></Route>
         </Routes>
       </BrowserRouter>
     </>
--- project-web-frontend/src/Components/Homepage/Homepage.js
+++ project-web-frontend/src/Components/Homepage/Homepage.js
@@ -1,8 +1,7 @@
-import React, {useState} from 'react';
-import styles from './Homepage.module.css';
-import { Link, useNavigate} from 'react-router-dom';
-import Enddate from './Enddate';
-
+import React, { useState } from "react";
+import styles from "./Homepage.module.css";
+import { Link, useNavigate } from "react-router-dom";
+import Enddate from "./Enddate";
 
 const Homepage = () => {
   // const navigate = useNavigate();
@@ -11,22 +10,20 @@ const Homepage = () => {
   //   navigate('/login');
   // };
 
-  const [startPointValue, setStartPointValue] = useState('Hà Nội');
-  const [endPointValue, setEndPointValue] = useState('TP. HCM');
+  const [startPointValue, setStartPointValue] = useState("Hà Nội");
+  const [endPointValue, setEndPointValue] = useState("TP. HCM");
 
-    const handleSwitch = () => {
-      setEndPointValue(startPointValue);
-      setStartPointValue(endPointValue);
-    };
+  const handleSwitch = () => {
+    setEndPointValue(startPointValue);
+    setStartPointValue(endPointValue);
+  };
 
   return (
-    <div style={{overflow: "hidden", height: "100vh"}}>
+    <div style={{ overflow: "hidden", height: "100vh" }}>
       <div className={styles.navbar}>
-        <div className={styles.headerLeft}>
-
-        </div>
+        <div className={styles.headerLeft}></div>
         <ul className={styles.headerRight}>
-          <Link to='/adminregister' style={{color: "white"}}>
+          <Link to="/registercoach" style={{ color: "white" }}>
             <li>Đăng ký mở bán vé</li>
           </Link>
           <div className={styles.signInButton}>
@@ -35,35 +32,44 @@ const Homepage = () => {
               Hotline 24/7
               <span></span>
             </button>
-            {
-              localStorage.getItem('token') ?
-                <div>
-                  <button className={styles.buttons} onClick={() => {
-                    localStorage.removeItem('token');
-                    window.location.replace('/');
-                  }}>
-                    Đăng xuất
-                    <span></span>
-                  </button> 
-                </div> :
-                <Link to='/login'>
-                  <button className={styles.buttons}>
-                    Đăng nhập
-                    <span></span>
-                  </button>
-                </Link>
-            }
+            {localStorage.getItem("token") ? (
+              <div>
+                <button
+                  className={styles.buttons}
+                  onClick={() => {
+                    localStorage.removeItem("token");
+                    window.location.replace("/");
+                  }}
+                >
+                  Đăng xuất
+                  <span></span>
+                </button>
+              </div>
+            ) : (
+              <Link to="/login">
+                <button className={styles.buttons}>
+                  Đăng nhập
+                  <span></span>
+                </button>
+              </Link>
+            )}
           </div>
         </ul>
       </div>
       <div className={styles.banner}>
-        <img className={styles.bannerBackground} src="https://229a2c9fe669f7b.cmccloud.com.vn/images/banner-main-vi.jpg"/>
+        <img
+          className={styles.bannerBackground}
+          src="https://229a2c9fe669f7b.cmccloud.com.vn/images/banner-main-vi.jpg"
+        />
         <div className={styles.bannerMiddle}>
           <h2>Đặt vé xe đơn giản và an toàn</h2>
           <div className={styles.ticketWidget}>
             <div className={styles.infoRow}>
-              <div className={styles.info} id={styles.startPoint} style={{position: "relative"}}>
-
+              <div
+                className={styles.info}
+                id={styles.startPoint}
+                style={{ position: "relative" }}
+              >
                 <div className={styles.switchButton} onClick={handleSwitch}>
                   <div className={styles.switchIcon}>
                     <i className="material-icons-outlined">import_export</i>
@@ -72,56 +78,62 @@ const Homepage = () => {
 
                 <div className={styles.selectStartPoint}>
                   <div className={styles.iconPoint}>
-                    <img className={styles.itemImg} src="https://229a2c9fe669f7b.cmccloud.com.vn/svgIcon/pickup_vex_blue_24dp.svg"/>
+                    <img
+                      className={styles.itemImg}
+                      src="https://229a2c9fe669f7b.cmccloud.com.vn/svgIcon/pickup_vex_blue_24dp.svg"
+                    />
                   </div>
                   <div className={styles.inputPointContainer}>
-                    <label className={styles.titlePoint}>
-                      Nơi xuất phát
-                    </label>
-                    <input 
-                      className={styles.inputPoint} 
-                      value={startPointValue} 
-                      onChange={(e) => setStartPointValue(e.target.value)}>
-                    </input>
+                    <label className={styles.titlePoint}>Nơi xuất phát</label>
+                    <input
+                      className={styles.inputPoint}
+                      value={startPointValue}
+                      onChange={(e) => setStartPointValue(e.target.value)}
+                    ></input>
                   </div>
                 </div>
               </div>
               <div className={styles.info} id={styles.endPoint}>
                 <div className={styles.selectEndPoint}>
-                    <div className={styles.iconPoint}>
-                      <img className={styles.itemImg} src="https://229a2c9fe669f7b.cmccloud.com.vn/svgIcon/dropoff_new_24dp.svg"/>
-                    </div>
-                    <div className={styles.inputPointContainer}>
-                      <label className={styles.titlePoint}>
-                        Nơi đến
-                      </label>
-                      <input 
-                        className={styles.inputPoint} 
-                        value={endPointValue} 
-                        onChange={(e) => setEndPointValue(e.target.value)}>
-                      </input>
-                    </div>
+                  <div className={styles.iconPoint}>
+                    <img
+                      className={styles.itemImg}
+                      src="https://229a2c9fe669f7b.cmccloud.com.vn/svgIcon/dropoff_new_24dp.svg"
+                    />
+                  </div>
+                  <div className={styles.inputPointContainer}>
+                    <label className={styles.titlePoint}>Nơi đến</label>
+                    <input
+                      className={styles.inputPoint}
+                      value={endPointValue}
+                      onChange={(e) => setEndPointValue(e.target.value)}
+                    ></input>
                   </div>
                 </div>
+              </div>
               <div className={styles.info} id={styles.startDate}>
                 <div className={styles.selectEndPoint}>
-                    <div className={styles.iconPoint}>
-                      <img className={styles.itemImg} src="https://storage.googleapis.com/fe-production/svgIcon/event_vex_blue_24dp.svg"/>
-                    </div>
-                    <div className={styles.inputPointContainer}>
-                      <label className={styles.titlePoint}>
-                        Ngày đi
-                      </label>
-                      <input className={styles.inputPoint} value="Ngày nào đó"></input>
-                    </div>
+                  <div className={styles.iconPoint}>
+                    <img
+                      className={styles.itemImg}
+                      src="https://storage.googleapis.com/fe-production/svgIcon/event_vex_blue_24dp.svg"
+                    />
                   </div>
+                  <div className={styles.inputPointContainer}>
+                    <label className={styles.titlePoint}>Ngày đi</label>
+                    <input
+                      className={styles.inputPoint}
+                      value="Ngày nào đó"
+                    ></input>
+                  </div>
+                </div>
               </div>
               <div className={styles.info} id={styles.endDate}>
                 <Enddate></Enddate>
               </div>
             </div>
             <div className={styles.searchButton}>
-              <Link to='/booking' style={{width: "100%"}}>
+              <Link to="/booking" style={{ width: "100%" }}>
                 <button className={styles.buttons} id={styles.searchButton}>
                   Tìm kiếm chuyến xe ngay
                   <span></span>
@@ -135,25 +147,41 @@ const Homepage = () => {
         <div className={styles.background}></div>
         <div className={styles.endItems}>
           <div className={styles.items}>
-            <img className={styles.itemImg} src="https://229a2c9fe669f7b.cmccloud.com.vn/svgIcon/verified_yellow.svg" alt="Verified" />
+            <img
+              className={styles.itemImg}
+              src="https://229a2c9fe669f7b.cmccloud.com.vn/svgIcon/verified_yellow.svg"
+              alt="Verified"
+            />
             <p className={styles.title}>Chắc chắn có chỗ</p>
           </div>
           <div className={styles.items}>
-            <img className={styles.itemImg} src="https://229a2c9fe669f7b.cmccloud.com.vn/svgIcon/headset_mic_yellow.svg" alt="Headset" />
+            <img
+              className={styles.itemImg}
+              src="https://229a2c9fe669f7b.cmccloud.com.vn/svgIcon/headset_mic_yellow.svg"
+              alt="Headset"
+            />
             <p className={styles.title}>Hỗ trợ 24/7</p>
           </div>
           <div className={styles.items}>
-            <img className={styles.itemImg} src="https://229a2c9fe669f7b.cmccloud.com.vn/svgIcon/discount_yellow.svg" alt="Discount" />
+            <img
+              className={styles.itemImg}
+              src="https://229a2c9fe669f7b.cmccloud.com.vn/svgIcon/discount_yellow.svg"
+              alt="Discount"
+            />
             <p className={styles.title}>Nhiều ưu đãi</p>
           </div>
           <div className={styles.items}>
-            <img className={styles.itemImg} src="https://229a2c9fe669f7b.cmccloud.com.vn/svgIcon/monetization_on_yellow.svg" alt="Payment" />
+            <img
+              className={styles.itemImg}
+              src="https://229a2c9fe669f7b.cmccloud.com.vn/svgIcon/monetization_on_yellow.svg"
+              alt="Payment"
+            />
             <p className={styles.title}>Thanh toán đa dạng</p>
           </div>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Homepage;
