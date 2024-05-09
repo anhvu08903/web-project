61ff0584b1df160086c6c5f261002166a5d4d9b2
ngoc vu <vungocanhdocac@gmail.com>
2024-05-09 23:00:25 +0700

    refix

--- project-web-backend/src/main/java/com/example/projectwebbackend/controllers/user/VNPayController.java
+++ project-web-backend/src/main/java/com/example/projectwebbackend/controllers/user/VNPayController.java
@@ -5,15 +5,12 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.UnsupportedEncodingException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-
+@CrossOrigin("http://localhost:3000")
 @RestController
 @RequestMapping("/users")
 public class VNPayController {
