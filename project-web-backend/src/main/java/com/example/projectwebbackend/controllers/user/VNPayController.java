4f4dbc9a4d1c79f09f25f5ebc18998fbd8d24723
ngoc vu <vungocanhdocac@gmail.com>
2024-05-09 22:44:06 +0700

    test vnpay api

---
+++ project-web-backend/src/main/java/com/example/projectwebbackend/controllers/user/VNPayController.java
@@ -0,0 +1,46 @@
+package com.example.projectwebbackend.controllers.user;
+
+import com.example.projectwebbackend.service.VNPayService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.UnsupportedEncodingException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@RestController
+@RequestMapping("/users")
+public class VNPayController {
+    @Autowired
+    private VNPayService vnPayService;
+    @GetMapping("/vnpay")
+    public ResponseEntity<String> submitPayment() throws UnsupportedEncodingException {
+        String vnpayUrl = vnPayService.getPay();
+        return ResponseEntity.ok(vnpayUrl);
+    }
+    @GetMapping("/vnpay-status")
+    public String GetMapping(HttpServletRequest request, Model model){
+        int paymentStatus =vnPayService.orderReturn(request);
+
+        String orderInfo = request.getParameter("vnp_OrderInfo");
+        String paymentTime = request.getParameter("vnp_PayDate");
+        String transactionId = request.getParameter("vnp_TransactionNo");
+        String totalPrice = request.getParameter("vnp_Amount");
+        // Định dạng lại thời gian thanh toán
+        LocalDateTime paymentDateTime = LocalDateTime.parse(paymentTime, DateTimeFormatter.ofPattern("yyyyMMddHHmmss"));
+        String formattedPaymentTime = paymentDateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+
+        model.addAttribute("orderId", orderInfo);
+        model.addAttribute("totalPrice", totalPrice);
+        model.addAttribute("paymentTime", formattedPaymentTime);
+        model.addAttribute("transactionId", transactionId);
+
+        return paymentStatus == 1 ? "ordersuccess" : "orderfail";
+    }
+}
